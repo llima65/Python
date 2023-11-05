0c93d6958c2615be4da982b80aba7d328cecd9f2
Lucasli6538 <121589431+llima65@users.noreply.github.com>
2023-11-05 14:52:33 -0300

    Create converterCemF.py

---
+++ converterCemF.py
@@ -0,0 +1,4 @@
+celsius = 37
+fahrenheit = (celsius * 1.8) + 32
+fahrenheitArredondado = round(fahrenheit, 2)
+print("A temperatura de ", celsius, " convertido em fahrenheit é ",fahrenheitArredondado)
