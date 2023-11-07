ba28543cd7e6c0ea5ffc34322d6b95349fc82ad4
Lucasli6538 <121589431+llima65@users.noreply.github.com>
2023-11-06 22:22:56 -0300

    Create informacao.py

---
+++ informacao.py
@@ -0,0 +1,4 @@
+nome = input('Qual o seu nome? ')
+idade = input('Qual a sua idade? ')
+altura = input('Qual a sua altura? ')
+print('Meu nome é '  ,nome  ,' tenho ',idade ,' anos e a minha altura é ', altura)
