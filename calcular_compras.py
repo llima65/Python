feefb0d9bdb513ed29c909bb5e30d66b31fe5518
lima <llima6538@gmail.com>
2024-01-04 19:57:58 -0300

    Update calcular_compras.py

--- calcular_compras.py
+++ calcular_compras.py
@@ -9,17 +9,18 @@ while 's' in resp or carteira <= 0:
     nome = str(input('Digite o nome do produto: ')).upper().strip()
     preco = float(input('Valor do produto: R$'))
     quat = int(input('Digite a quantidade: '))
-    resp = str(input('Quer continhuar? ')).strip().lower()
+    
     totpreco += preco * quat
     restante = carteira - totpreco
     extrato = (f'Você colocou no carrinho total R$ {totpreco:.2f}\n'
                f'Seu saldo atual é de {restante:.2f}')
-    print('='*20)
+    
     print(extrato)
 
     if restante <= 0:
         break
-
+    print('='*30)
+    resp = str(input('Quer continhuar? ')).strip().lower()
 if restante <= 0:
      print('Você não tem dinheiro suficiente para compras todos os itens adicionado')
      print('Por favor. Tire alguns itens do carrinho.')
